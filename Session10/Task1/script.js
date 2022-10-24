29c40fd97dd211aec0c36ef0dd5fd0c961ec3641
jdcsjdcs <csjanosdeak@gmail.com>
2022-10-24 18:07:58 +0100

    session10

---
+++ Session10/Task1/script.js
@@ -0,0 +1,7 @@
+function turn_on() {
+    document.getElementById("bulb").src = "pic_bulbon.gif";
+}
+
+function turn_off() {
+    document.getElementById("bulb").src = "pic_bulboff.gif";
+}
---
+++ Session10/Task2/script.js
@@ -0,0 +1,6 @@
+function bmi() {
+    let w = document.getElementById("weight").value;
+    let h = document.getElementById("height").value;
+    let bmi = w / (h * h / 10000)
+    document.getElementById("bmi").innerHTML = "Your BMI is: " + bmi
+};

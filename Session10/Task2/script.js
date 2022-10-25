aa6265b6dfd6d8ec73485397bb6fa333a045eae5
jdcsjdcs <csjanosdeak@gmail.com>
2022-10-25 09:12:19 +0100

    session10

--- Session10/Task2/script.js
+++ Session10/Task2/script.js
@@ -1,6 +1,7 @@
-function bmi() {
-    let w = document.getElementById("weight").value;
-    let h = document.getElementById("height").value;
-    let bmi = w / (h * h / 10000)
-    document.getElementById("bmi").innerHTML = "Your BMI is: " + bmi;
-};
+function turn_on() {
+    document.getElementById("pm").src = "https://thumbs.dreamstime.com/b/rishi-sunak-outside-no-downing-street-london-uk-chancellor-exchequer-leaves-to-present-his-budget-house-commons-188516418.jpg";
+}
+
+function turn_off() {
+    document.getElementById("pm").src = "https://thumbs.dreamstime.com/b/london-united-kingdom-march-liz-truss-arrives-cabinet-meeting-downing-street-london-uk-cabinet-meeting-downing-201830221.jpg";
+}

bd3be0d9ee6118a85c4c0221d598cc13ce23bcc9
jdcsjdcs <csjanosdeak@gmail.com>
2022-10-27 14:00:48 +0100

    a

--- Session12/Task/script.js
+++ Session12/Task/script.js
@@ -1,13 +1,25 @@
 function price() {
     document.querySelector('.cong_pic').src = 'beer.jpg'
-    const price_btn = document.querySelector('#price_btn');
+    let price_btn = document.querySelector('#price_btn');
     price_btn.style.display = 'none';
+
     let h3 = document.createElement('h3');
     h3.innerText = 'Enjoy!';
     document.body.append(h3);
     document.body.style.backgroundColor = '#a8dadc';
 
-}
+    let p = document.createElement('p');
+    p.innerText = 'Some more?';
+    document.body.append(p);
 
+    let yes_btn = document.createElement('button');
+    yes_btn.innerText = 'yes, please';
+    document.body.append(yes_btn);
+    // yes_btn.onclick = document.body.append(beer.jpg);
+
+    let no_btn = document.createElement('button');
+    no_btn.innerText = 'no, thanks';
+    document.body.append(no_btn);
+}
 
 
